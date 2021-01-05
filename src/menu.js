c55916688703108878773a5397bfef616d892440
thunder-17 <52612051+thunder-17@users.noreply.github.com>
2021-01-05 01:26:17 +0100

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -1,17 +1,26 @@
-const { TextChannel } = require('discord.js')
+const {
+    TextChannel
+} = require('discord.js')
 
 module.exports = class Menu {
-    static defaultReactions = { first: '⏪', back: '◀', next: '▶', last: '⏩', stop: '⏹' }
+    static defaultReactions = {
+        back: '⬅️',
+        next: '➡️',
+        stop: '⏹'
+    }
 
     constructor(opts = {}) {
-        const { channel = new TextChannel, userID, pages = [], page = 0, time = 120000, reactions = Menu.defaultReactions, customCatch = console.error } = opts
+        const {
+            channel = new TextChannel, message, userID, pages = [], page = 0, time = 120000, reactions = Menu.defaultReactions, customCatch = console.error
+        } = opts
         this.channel = channel
+        this.message = message
         this.pages = pages
         this.time = time
         this.reactions = reactions
         this.page = page
         this.catch = customCatch
-        channel.send(pages[page]).then(msg => {
+        message.reply(pages[page]).then(msg => {
             this.msg = msg
             this.addReactions()
             this.createCollector(userID)
@@ -22,17 +31,15 @@ module.exports = class Menu {
         this.msg.edit(this.pages[pg]).catch(this.catch)
     }
     createCollector(uid) {
-        const collector = this.msg.createReactionCollector((r, u) => u.id == uid, { time: this.time })
+        const collector = this.msg.createReactionCollector((r, u) => u.id == uid, {
+            time: this.time
+        })
         this.collector = collector;
         collector.on('collect', r => {
-            if (r.emoji.name == this.reactions.first) {
-                if (this.page != 0) this.select(0)
-            } else if (r.emoji.name == this.reactions.back) {
+            if (r.emoji.name == this.reactions.back) {
                 if (this.page != 0) this.select(this.page - 1)
             } else if (r.emoji.name == this.reactions.next) {
                 if (this.page < this.pages.length - 1) this.select(this.page + 1)
-            } else if (r.emoji.name == this.reactions.last) {
-                if (this.page != this.pages.length) this.select(this.pages.length - 1)
             } else if (r.emoji.name == this.reactions.stop) collector.stop()
             r.users.remove(uid).catch(this.catch)
         })
@@ -42,11 +49,9 @@ module.exports = class Menu {
     }
     async addReactions() {
         try {
-            if (this.reactions.first) await this.msg.react(this.reactions.first)
-            if (this.reactions.back)  await this.msg.react(this.reactions.back)
-            if (this.reactions.next)  await this.msg.react(this.reactions.next)
-            if (this.reactions.last)  await this.msg.react(this.reactions.last)
-            if (this.reactions.stop)  await this.msg.react(this.reactions.stop)
+            if (this.reactions.back) await this.msg.react(this.reactions.back)
+            if (this.reactions.next) await this.msg.react(this.reactions.next)
+            if (this.reactions.stop) await this.msg.react(this.reactions.stop)
         } catch (e) {
             this.catch(e)
         }
